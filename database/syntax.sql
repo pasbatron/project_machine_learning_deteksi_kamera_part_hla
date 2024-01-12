4481208b6ed88246e79a82831665874e255acdb3
Pasbatron <131593509+pasbatron@users.noreply.github.com>
2024-01-12 13:39:13 +0700

    Update syntax.sql

--- database/syntax.sql
+++ database/syntax.sql
@@ -1,1 +1,10 @@
-Create
+CREATE TABLE table_adjuster_valve_hla (
+	id INTEGER PRIMARY KEY,
+	name_line TEXT NOT NULL,
+	name_part TEXT NOT NULL,
+	quantity TEXT NOT NULL ,
+	delay TEXT NOT NULL,
+  status TEXT NOT NULL,
+  andon TEXT NOT NULL,
+  datetime DEFAULT (DATETIME('now', 'localtime'))
+);
