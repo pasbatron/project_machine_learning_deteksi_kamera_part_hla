8b0280fe4a00a913bf782dc87533261cf764008a
Pasbatron <131593509+pasbatron@users.noreply.github.com>
2024-02-09 14:26:45 +0700

    Update program_arduino.ino

--- program_arduino/program_arduino.ino
+++ program_arduino/program_arduino.ino
@@ -2,11 +2,14 @@
 #include <SoftwareSerial.h>
 
 int x; 
-const byte rxPinDfplayer = 2;
-const byte txPinDfplayer = 3;
-const byte txPinLora = 10;
-const byte rxPinLora = 11;
-const int pinLed = 21;
+const byte rxPinDfplayer  = 2;
+const byte txPinDfplayer  = 3;
+const byte txPinLora      = 10;
+const byte rxPinLora      = 11;
+const int pinLedKuning    = 4;
+const int pinLedMerah     = 12;
+const int pinLedBiru      = 8;
+const int pinLedHijau     = 13;
 
 SoftwareSerial lora(txPinLora, rxPinLora);
 SoftwareSerial dfplayer(rxPinDfplayer, txPinDfplayer);
@@ -17,41 +20,39 @@ void setup() {
 	Serial.begin(9600); 
   dfplayer.begin(9600);
   lora.begin(9600);
-
 	Serial.setTimeout(1); 
   myDFPlayer.begin(dfplayer);
-  myDFPlayer.volume(60);
-  myDFPlayer.play(1);
-  myDFPlayer.read();
-  
+  // myDFPlayer.volume(40);
+  // myDFPlayer.play(2);
+  // myDFPlayer.read();
+
   pinMode(rxPinDfplayer, INPUT);
   pinMode(txPinDfplayer, OUTPUT);
-  pinMode(pinLed, OUTPUT);
-  digitalWrite(pinLed, LOW);
+  pinMode(pinLedKuning, OUTPUT);
+  pinMode(pinLedMerah, OUTPUT);
+  pinMode(pinLedHijau, OUTPUT);
+  pinMode(pinLedBiru, OUTPUT);
 
+  digitalWrite(pinLedKuning, HIGH);
+  digitalWrite(pinLedMerah, HIGH);
+  digitalWrite(pinLedHijau, HIGH);
+  digitalWrite(pinLedBiru, HIGH);
   delay(1000);
 } 
 
 void loop() { 
+  digitalWrite(pinLedKuning, LOW);
+  digitalWrite(pinLedHijau, HIGH);
   while (!Serial.available()); 
   x = Serial.readString().toInt();
     if(x == 1){
-      myDFPlayer.play(1);
+      myDFPlayer.play(2);
       myDFPlayer.read();
-      digitalWrite(pinLed, HIGH);
-      delay(3000);
-      digitalWrite(pinLed, LOW);
-      lora.println("Data Dari Kamera"); 
-      delay(1000);
-    }if(x == 2){
-      myDFPlayer.play(1);
-      myDFPlayer.read();
-      digitalWrite(pinLed, HIGH);
-      delay(3000);
-      digitalWrite(pinLed, LOW);
-      lora.println("Data Dari Kamera"); 
-      delay(1000);
-    }else {
-      digitalWrite(pinLed, LOW);
+      digitalWrite(pinLedHijau, LOW);
+      digitalWrite(pinLedKuning, HIGH);
+      lora.println("Data Dari Kamera");
+      delay(6000);
+      digitalWrite(pinLedHijau, LOW);
+      digitalWrite(pinLedKuning, LOW);
     }
 } 
